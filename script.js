8ee0b8abde036b97049346181d19dc91eb8cacef
elahehgh68 <elaheh.ghoreishi68@gmail.com>
2026-08-01 16:14:36 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//
+const cover = document.getElementById("cover");
+const invite = document.getElementById("invite");
+
+cover.addEventListener("click", function () {
+
+    cover.style.display = "none";
+
+    invite.style.display = "block";
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
