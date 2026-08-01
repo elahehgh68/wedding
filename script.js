45e2935b327d9e5fd3743afa0f21a192489f2a03
elahehgh68 <elaheh.ghoreishi68@gmail.com>
2026-08-01 18:08:36 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+const envelope = document.getElementById("envelope");
 const card = document.getElementById("card");
+const cover = document.getElementById("coverImage");
+const info = document.getElementById("info");
 
-card.addEventListener("click", function () {
-    card.classList.toggle("open");
-});
+let opened = false;
+
+// کلیک روی پاکت
+envelope.onclick = function () {
+
+    if(opened) return;
+
+    opened = true;
+
+    document.getElementById("scene").classList.add("open");
+
+};
+
+
+// کلیک روی خود کارت
+card.onclick = function(){
+
+    if(!opened) return;
+
+    cover.style.display="none";
+
+    info.style.display="block";
+
+};
